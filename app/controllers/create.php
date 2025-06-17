82b55887d73de9a2bbe7323dd09363cf38515299
TahjP <tplummer@algomau.ca>
2025-06-17 20:16:30 +0000

    Created page to create account

--- app/controllers/create.php
+++ app/controllers/create.php
@@ -5,4 +5,13 @@ class Create extends Controller {
     public function index() {		
 	    $this->view('create/index');
     }
+  public function store() {
+      $username = strtolower(trim($_REQUEST['username']));
+      $password = $_REQUEST['password'];
+
+      $user = $this->model('User');
+      $message = $user->register($username, $password);
+
+      $this->view('create/index', ['message' => $message]);
+  }
 }
