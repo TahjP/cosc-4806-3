649112c8cececf96a9b68dda8393228cbab1055c
TahjP <tplummer@algomau.ca>
2025-06-20 04:23:05 +0000

    The register method was implemented in the user model

--- app/models/User.php
+++ app/models/User.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once('database.php');
+
 class User {
 
     public $username;
@@ -45,6 +47,32 @@ class User {
 			header('Location: /login');
 			die;
 		}
+    }
+    public function register($username, $password) {
+        $db = db_connect();
+
+        if (strlen($password) < 10) {
+            return "Password must be at least 10 characters long.";
+        }
+
+        $check = $db->prepare("SELECT * FROM users WHERE username = :username;");
+        $check->execute(['username' => $username]);
+
+        if ($check->fetch()) {
+            return "Error: Username already exists.";
+        }
+
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        $stmt = $db->prepare("INSERT INTO users (username, password) VALUES (:username, :password);");
+        $stmt->execute([
+            'username' => $username,
+            'password' => $hashedPassword
+        ]);
+
+        $_SESSION['auth'] = 1;
+        $_SESSION['username'] = ucwords($username);
+        header('Location: /home');
+        exit;
     }
 
 }
